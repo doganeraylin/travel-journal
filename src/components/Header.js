ed5c15a4bcc0dc605f994395aacbea71e06aee40
aylindoganer <doganeraylinn@gmail.com>
2022-10-20 17:00:14 +0300

    added class names

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import headerIcon from "../assets/headerIcon.png"
 export default function Header() {
     return (
         <nav>
-            <img src={headerIcon}></img>
-            <h1>my travel journal</h1>
+            <img src={headerIcon} className="header-logo"></img>
+            <h1 className="header-title">my travel journal.</h1>
         </nav>
     )
 }
