0c57c3360d3efed29ec195e48e9030d54c24ab04
aylindoganer <doganeraylinn@gmail.com>
2022-10-20 16:59:58 +0300

    added class names

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,18 +5,18 @@ import mapIcon from "../assets/mapIcon.svg"
 export default function Card(props) {
     return (
         <div className="card-container">
-            <div className="card-img">
-                <img src={props.imageUrl}/>
+            <div className="card-img-container">
+                <img src={props.imageUrl} className="card-img"/>
             </div>
-            <div className="card-info">
-                <div className="location">
-                    <img src={mapIcon}/>
-                    <p>{props.location}</p>
-                    <a href={props.googleMapsUrl}>View on Google Maps</a>
+            <div className="card-info-container">
+                <div className="location-info">
+                    <img src={mapIcon} className="map-icon"/>
+                    <p className="location">{props.location}</p>
+                    <a href={props.googleMapsUrl} className="google-maps">View on Google Maps</a>
                 </div>
-                <p>{props.title}</p>
-                <p>{props.startDate} - {props.endDate}</p>
-                <p>{props.description}</p>
+                <p className="title">{props.title}</p>
+                <p className="dates">{props.startDate} - {props.endDate}</p>
+                <p className="description">{props.description}</p>
             </div>
         </div>
     )
